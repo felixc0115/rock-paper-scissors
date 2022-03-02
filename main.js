d4766bddb4710c163ac07610a123bc2bb8d0eb25
Felix Chen <fechen1993@gmail.com>
2022-03-02 11:06:12 -0500

    add interactivity to game

--- main.js
+++ main.js
@@ -1,4 +1,32 @@
 
+let round = 0;
+let playerScore = 0;
+let computerScore = 0;
+
+const buttons = document.querySelectorAll('.individual-button');
+buttons.forEach(button => button.addEventListener('click', function () {
+    button.classList.toggle('playing');
+    let playerSelection = button.textContent;
+    let computerSelection = computerPlay();
+    let roundResult = compareSelections(playerSelection, computerSelection);
+    game(playerSelection, computerSelection, roundResult);
+}))
+
+const playerChoiceOutput = document.querySelector('.player-choice-output');
+const computerChoiceOutput = document.querySelector('.computer-choice-output');
+const roundResultOutput = document.querySelector('.round-result-output');
+const currentScoreOutput = document.querySelector('.current-score-output');
+const roundOutput = document.querySelector('.round-output');
+const gameResultOutput = document.querySelector('.game-result-output');
+
+function removeTransition (e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+buttons.forEach(button => button.addEventListener('transitionend', removeTransition));
+
+
 //Randomly generate the computer's choice 
 function computerPlay() {
     let computerSelection = Math.floor(Math.random() * 3) + 1;
@@ -13,54 +41,51 @@ function computerPlay() {
     }
 }
 
-//Compare user and computer choices
-function playRound(playerSelection, computerSelection) {
+//Compare player and computer choices
+function compareSelections(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return 'draw';
     }
     else if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'rock')) {
-        return 'user';
+        return 'player';
     }
     else {
         return 'computer';
     }
 }
-//Declare winner if there is one; if there is no winner then go again. There will be a total of five rounds.
-function game() {
-    let userScore = 0;
-    let computerScore = 0;
-    for (let i = 1; i <= 5; i++) {
-        console.log(`This is round ${i}`);
-        let playerSelection = prompt("Enter rock, paper, or scissors"); //need to make it so any other string entered will prompt the user repeatedly until a valid one is entered
-        console.log(`Player chose: ${playerSelection}`);
-        let computerSelection = computerPlay();
-        console.log(`Computer chose: ${computerSelection}`);
-        const roundResult = playRound(playerSelection, computerSelection);
-        if (roundResult === 'user') {
-            userScore += 1;
-            console.log(`You win ${playerSelection} beats ${computerSelection}`);
-            console.log(`The current score is: user - ${userScore} and computer - ${computerScore}`);
-        }
-        if (roundResult === 'draw') {
-            userScore += 0;
-            console.log(`It is a draw. Both players picked ${playerSelection}`);
-            console.log(`The current score is: user - ${userScore} and computer - ${computerScore}`);
-        }
-        else {
-            computerScore += 1;
-            console.log(`You lose ${computerSelection} beats ${playerSelection}`);
-            console.log(`The current score is: user - ${userScore} and computer - ${computerScore}`);
-        }
-    }
-    if (userScore > computerScore) {
-        return 'user has won';
-    }
-    else if (userScore < computerScore) {
-        return 'computer has won';
-    }
 
+function game(playerSelection, computerSelection, roundResult) {
+    round += 1;
+    roundOutput.textContent = `Round ${round}`;
+    playerChoiceOutput.textContent = `Player chose ${playerSelection}`;
+    computerChoiceOutput.textContent = `Computer chose ${computerSelection}`;
+
+    if (roundResult === 'player') {
+        playerScore += 1;
+        roundResultOutput.textContent = 'Player has won!';
+    }
+    if (roundResult === 'draw') {
+        playerScore += 0;
+        roundResultOutput.textContent = `It is a draw. Both players picked ${playerSelection}`;
+    }
     else {
-        return 'draw - nobody won';
+        computerScore += 1;
+        roundResultOutput.textContent = 'Computer has won :(';
     }
-}
 
+    currentScoreOutput.textContent = `The current score is: player - ${playerScore} and computer - ${computerScore}`;
+    gameResultOutput.style.display = 'none';
+    if (playerScore === 5 || computerScore === 5) {
+        if (playerScore === 5) {
+            gameResultOutput.style.display = '';
+            gameResultOutput.textContent = 'Player has won the game!!';
+        } 
+        else if (computerScore === 5) {
+            gameResultOutput.style.display = '';
+            gameResultOutput.textContent = 'Computer has won the game :(';
+        }
+        playerScore = 0;
+        computerScore = 0;
+        round = 0;
+    }
+    }
